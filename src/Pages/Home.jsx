41e2810950066ae85f20ebfd9f291a67644f8120
shah1012 <daxshah1203@gmail.com>
2022-03-09 11:02:02 -0600

    Initial Setup

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,16 @@
+import React, {useState} from 'react'
+
+import {motion} from "framer-motion"
+
+const Home = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  return (
+    <motion.div animate={{
+
+    }
+    } >Home</motion.div>
+  )
+}
+
+export default Home
