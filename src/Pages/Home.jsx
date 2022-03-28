28ad7e38ccdbc883d1eb24fab8efd098c0eab0c6
dylanmas <dylanmas687@gmail.com>
2022-03-28 11:00:35 -0500

    Some progress on home screen

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState } from "react";
+import Background from "../components/Background";
+import TitleCard from "../components/TitleCard";
+import HomepageLogo from "../images/HomepageLogo.png";
+import InputComponent from "../components/InputComponent";
+import Button from "../components/Button";
+import InputForm from "../components/InputForm";
+import LinkTag from "../components/LinkTag";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <Background>
+      <div className="wrapper" style={{ display: "flex", justifyContent: "space-evenly", width: "100%" }}>
+        <img
+          src={HomepageLogo}
+          style={{
+            objectFit: "contain",
+            width: "120px",
+            marginTop: "30px",
+          }}
+        />
+        <TitleCard title={"Cardle.io"} />
+        <img
+          src={HomepageLogo}
+          style={{
+            objectFit: "contain",
+            width: "120px",
+            marginTop: "30px",
+          }}
+        />
+      </div>
+      <Button text={"Login"} />
+    </Background>
+  );
+};
 
-export default Home
+export default Home;
