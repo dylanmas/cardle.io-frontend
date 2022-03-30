ccd8abb06e96e5e73602fab2deb5fc19b2b26228
shah1012 <daxshah1203@gmail.com>
2022-03-30 11:01:38 -0500

    Home Page Progress

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,35 +1,22 @@
 import React, { useState } from "react";
+import "../styles/home.css"
 import Background from "../components/Background";
 import TitleCard from "../components/TitleCard";
 import HomepageLogo from "../images/HomepageLogo.png";
-import InputComponent from "../components/InputComponent";
-import Button from "../components/Button";
-import InputForm from "../components/InputForm";
-import LinkTag from "../components/LinkTag";
+import HomepageLogo2 from "../images/HomepageLogo2.png";
+import Leaderboard from "../components/Leaderboard";
 
 const Home = () => {
   return (
     <Background>
-      <div className="wrapper" style={{ display: "flex", justifyContent: "space-evenly", width: "100%" }}>
-        <img
-          src={HomepageLogo}
-          style={{
-            objectFit: "contain",
-            width: "120px",
-            marginTop: "30px",
-          }}
-        />
+      <div className="header">
+        <img src={HomepageLogo2} alt="" className="leftImg" />
         <TitleCard title={"Cardle.io"} />
-        <img
-          src={HomepageLogo}
-          style={{
-            objectFit: "contain",
-            width: "120px",
-            marginTop: "30px",
-          }}
-        />
+        <img src={HomepageLogo} alt="" className="rightImg" />
       </div>
-      <Button text={"Login"} />
+
+      <Leaderboard />
+      
     </Background>
   );
 };
---
+++ src/components/Leaderboard.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import "../styles/Leaderboard.css"
+
+const Leaderboard = () => {
+  return (
+    <div className="lbContainer">
+        <h3>Todays Score: </h3>
+       <ul className="lbList">
+           <li className="rank">Test</li>
+           <li className="rank">Test</li>
+           <li className="rank">Test</li>
+           <li className="rank">Test</li>
+           <li className="rank">Test</li>
+           <li className="rank">Test</li>
+       </ul>
+
+    </div>
+  )
+}
+
+export default Leaderboard
