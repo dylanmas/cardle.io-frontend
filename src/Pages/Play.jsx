b9454a644640b1e7a1132c183fe061f9da18cf1d
shah1012 <daxshah1203@gmail.com>
2022-04-07 11:03:12 -0500

    added playing cards

--- src/Pages/Play.jsx
+++ src/Pages/Play.jsx
@@ -1,10 +1,22 @@
 import React, { useEffect, useState } from "react";
 import Background from "../components/Background";
 import TimerCard from "../components/TImerCard";
+import '../styles/play.css'
+import Card from "react-free-playing-cards"
+
 
 const Play = () => {
   const [time, setTime] = useState("");
-  const [stopTime, setStopTime] = useState(false);
+  const [stopTime, setStopTime] = useState(true);
+  const [cardsArray, setCardsArray] = useState([])
+
+  const defHandStyle = {
+    maxHeight: "34vh",
+    minHeight: "34vh",
+
+    maxWidth: "100vw",
+    padding: 0,
+  };
 
   useEffect(() => {
     let secs = 0;
@@ -27,7 +39,6 @@ const Play = () => {
       }
       if (secs < 10) {
         secsStr = "0" + secs.toString();
-        console.log(secs)
       } else {
         secsStr = secs;
       }
@@ -49,9 +60,32 @@ const Play = () => {
     }
   }, []);
 
+  useEffect(() => {
+    placeCards()
+  }, [])
+
+
+
+  const placeCards = () => {
+      let temp = [];
+      const suits = ['c', 'd', 'h', 's']
+      const ranks = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A']
+      for(let i=0; i<suits.length; i++){
+          for(let j=0; j<ranks.length; j++){
+            temp = [...temp, {type: `${ranks[j]}${suits[i]}`}]
+          }
+      }
+    setCardsArray(temp)    
+  }
+
   return (
     <Background>
       <TimerCard time={`Time: ${time}`} />
+      <div className="cardHolder">
+        {cardsArray.map((card, i) => (
+            <Card card={card.type} key={i} className="card"/>
+        ))}
+      </div>
     </Background>
   );
 };
