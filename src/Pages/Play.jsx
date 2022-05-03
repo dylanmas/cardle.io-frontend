e1819ce4908bb8b091f15592eb5b55cc39d59c50
shah1012 <daxshah1203@gmail.com>
2022-05-03 10:47:15 -0500

    working on randomizing cards

--- src/Pages/Play.jsx
+++ src/Pages/Play.jsx
@@ -1,26 +1,23 @@
 import React, { useEffect, useState } from "react";
 import Background from "../components/Background";
 import TimerCard from "../components/TImerCard";
-import '../styles/play.css'
-import Card from "react-free-playing-cards"
-
+import "../styles/play.css";
+import Card from "react-free-playing-cards";
 
 const Play = () => {
   const [time, setTime] = useState("");
   const [stopTime, setStopTime] = useState(true);
-  const [cardsArray, setCardsArray] = useState([])
-  const [lives, setLives] = useState(0)
-  const [rcard, setRCard] = useState({})
-
+  const [cardsArray, setCardsArray] = useState([]);
+  const [lives, setLives] = useState(0);
 
   useEffect(() => {
     let secs = 0;
     let mins = 0;
     let hrs = 0;
 
-    let secsStr = 0
-    let minsStr = 0
-    let hrsStr = 0
+    let secsStr = 0;
+    let minsStr = 0;
+    let hrsStr = 0;
 
     let interval = setInterval(() => {
       secs++;
@@ -38,12 +35,12 @@ const Play = () => {
         secsStr = secs;
       }
       if (mins < 10) {
-        minsStr = "0" + mins.toString()
+        minsStr = "0" + mins.toString();
       } else {
         minsStr = mins;
       }
       if (hrs < 10) {
-        hrsStr = "0" + hrs.toString()
+        hrsStr = "0" + hrs.toString();
       } else {
         hrsStr = hrs;
       }
@@ -56,34 +53,62 @@ const Play = () => {
   }, []);
 
   useEffect(() => {
-    placeCards()
-  }, [])
+    placeCards();
+  }, []);
 
- 
-  const cardClicked = (e) => {
-    console.log(e)
-  }
+  useEffect(() => {
+    if (cardsArray.length > 0) {
+      const {rCard, rIndex } = pickRandomCard();
+      
+    }
+  }, [cardsArray]);
 
-
+  const pickRandomCard = () => {
+    const rArr = [...cardsArray];
+    const rIndex = Math.floor(Math.random() * rArr.length);
+    const rCard = { ...rArr[rIndex], random: true };
+    rArr[rIndex] = rCard;
+    return {rCard, rIndex}
+  };
 
   const placeCards = () => {
-      let temp = [];
-      const suits = ['c', 'd', 'h', 's']
-      const ranks = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A']
-      for(let i=0; i<suits.length; i++){
-          for(let j=0; j<ranks.length; j++){
-            temp = [...temp, {type: `${ranks[j]}${suits[i]}`}]
-          }
+    let temp = [];
+    const suits = ["c", "d", "h", "s"];
+    const ranks = [
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "T",
+      "J",
+      "Q",
+      "K",
+      "A",
+    ];
+    for (let i = 0; i < suits.length; i++) {
+      for (let j = 0; j < ranks.length; j++) {
+        temp = [...temp, { type: `${ranks[j]}${suits[i]}` }];
       }
-    setCardsArray(temp)    
-  }
+    }
+    setCardsArray(temp);
+  };
+
+  const onCardClicked = (e) => {
+    console.log(e);
+  };
 
   return (
     <Background>
       <TimerCard time={`Time: ${time}`} />
       <div className="cardHolder">
         {cardsArray.map((card, i) => (
-            <Card card={card.type} key={i} className="card" onClick={cardClicked}/>
+          <div className={`cd ${card.type}`} key={i} onClick={onCardClicked}>
+            <Card card={card.type} key={i} className="card" />
+          </div>
         ))}
       </div>
     </Background>
